8168b67f2764e8fcc128e91f0e4b0da3c6aa8cd7
abhi <s530670@nwmissouri.edu>
2018-11-03 14:59:59 -0500

    Added cors

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ from bson.json_util import dumps
 from bson.objectid import ObjectId
 import io
 import csv
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 app.config["MONGO_URI"] = "mongodb+srv://admin:admin@cluster0-znwk6.mongodb.net/demo?retryWrites=true"
 mongo = PyMongo(app)
 
